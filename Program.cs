5e7cbba12c7322a28e1f60e65189db506188e182
hphuccntt <phuc54212420014@bachkhoasaigon.edu.vn>
2025-02-10 09:04:27 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write(" Chao Toi Ten La :");
+        string ten = Console.ReadLine();
+        Console.WriteLine("Toi Ten :" + ten);
+        Console.ReadKey();
+
+        /*
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine(" Nhap So Thu Nhat! ");
+        double sothunhat = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(" Nhap So Thu Hai! ");
+        double sothuhai = Convert.ToDouble(Console.ReadLine());
+        double tong = sothunhat + sothuhai;
+        Console.WriteLine(" Tong cua hai so la :" + tong) ;
+        Console.WriteLine("{0} + {1} = {2}",  sothunhat, sothuhai, tong);
+        */
+        
+
+    }
+}
